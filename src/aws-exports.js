0c1c09ae0d63a11fe65a2ff9e05448e73d1fb668
Lokendra Singh <lokendrasingh2000@yahoo.com>
2020-09-19 00:15:57 +0530

    Test

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -4,8 +4,8 @@
 const awsmobile = {
     "aws_project_region": "us-east-2",
     "aws_cognito_region": "us-east-2",
-    "aws_user_pools_id": "us-east-2_rePxRzoBq",
-    "aws_user_pools_web_client_id": "3nph7s5rqjujggtohmjqk69cdg",
+    "aws_user_pools_id": "us-east-2_OAkSRUyht",
+    "aws_user_pools_web_client_id": "5sp8spr63iv5kdf8p515kpaepf",
     "oauth": {}
 };
 
